b41efe219671a553d84520e8ba8da606f9ab5f96
Vladislav Likhachev <mymindissharp@gmail.com>
2021-10-19 21:47:22 +0300

    Added solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String text = "The Hobbit is a film series consisting of three high fantasy adventure films directed by Peter Jackson " +
+                "The three films are The Hobbit An Unexpected Journey The Hobbit The Desolation of Smaug and " +
+                "The Hobbit The Battle of the Five Armies The films are based on the novel The Hobbit by Tolkien " +
+                "with large portions of the trilogy inspired by the appendices to The Return of the King " +
+                "which expand on the story told in The Hobbit as well as new material and characters written especially for the films " +
+                "Together they act as a prequel to Jackson The Lord of the Rings film trilogy";
+
+        Function<String, List<String>> dictionaryGenerator = string -> Arrays.stream((string).split(" "))
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+
+        dictionaryGenerator.apply(text).forEach(System.out::println);
     }
 }
+
